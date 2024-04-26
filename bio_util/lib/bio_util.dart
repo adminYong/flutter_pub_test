d95ca2f4ade098636af2650cc4ae24158404515d
adminYong <168169120+adminYong@users.noreply.github.com>
2024-04-26 17:20:25 +0800

    Add files via upload

---
+++ bio_util/lib/bio_util.dart
@@ -0,0 +1,12 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/bio_util_base.dart';
+
+export 'src/bio_util_test.dart';
+
+export 'package:logger/logger.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ bio_util/lib/src/bio_util_test.dart
@@ -0,0 +1,7 @@
+import 'package:logger/logger.dart';
+
+class BioUtilTest {
+  static printStr() {
+    Logger().e("11111111");
+  }
+}
